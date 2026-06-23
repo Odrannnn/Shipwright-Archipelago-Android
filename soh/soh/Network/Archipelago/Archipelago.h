aebd22d58211a9b367461d029a2726d2e11090d3
Jerom Venneker <jerom.venneker@gmail.com>
2026-06-23 20:29:37 +0200

    Bump ap version

--- soh/soh/Network/Archipelago/Archipelago.h
+++ soh/soh/Network/Archipelago/Archipelago.h
@@ -19,7 +19,7 @@ static constexpr int MAX_PASSWORD_LENGTH = 32;
 
 static constexpr char const* AP_GAME_NAME = "Ship of Harkinian";
 static constexpr char const* AP_WORLD_VERSION_MAJOR = "1";
-static constexpr char const* AP_WORLD_VERSION_MINOR = "3";
+static constexpr char const* AP_WORLD_VERSION_MINOR = "4";
 static constexpr int MAX_RETRIES = 3;
 } // namespace AP_Client_consts
 
